0ad03acf9ea4b71515736bf15458606c63924b15
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2022-05-15 00:53:57 +0800

    Create basic_gui.py

---
+++ basic_gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+window = tk.Tk()
+#The first parameter of the Label call is the name of the parent window
+#keyword parameter "text" specifies the text to be shown:
+
+#tk.Label(root, text="Hello Tkinter!")
+
+
+#system pause
+window.mainloop()
